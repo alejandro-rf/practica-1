4084c8dc9d1c2b00da1676abe99e955cdc7b1f3a
alejandro-rf <arenones@edu.tecnocampus.cat>
2022-01-19 22:52:28 +0100

    Add TestObject

--- Assets/_Scripts/Test.cs
+++ Assets/_Scripts/Test.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField]
+    int test = 5;
     // Start is called before the first frame update
     void Start()
     {
