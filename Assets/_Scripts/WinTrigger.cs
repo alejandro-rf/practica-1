8b28cbb459177a4307dd4c27ecc6ddcbee9b701d
alejandro-rf <arenones@edu.tecnocampus.cat>
2022-01-19 22:34:56 +0100

    Add project files

---
+++ Assets/_Scripts/WinTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinTrigger : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject WinObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var player = other.GetComponent<PlayerMovement>();
+        if (player)
+        {
+            ShowWin();
+            Destroy(gameObject);
+        }
+    }
+
+    private void ShowWin()
+    {
+        WinObject.SetActive(true);
+    }
+}
