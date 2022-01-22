9efc92f5ce63eaa7679fdee04f37e22d79099698
Animorama <jmora.aparicio@gmail.com>
2022-01-22 11:11:59 +0100

    Player jumper + groundChecker Base scripts

---
+++ Assets/_Scripts/GroundChecker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public Transform GroundCheckPoint;
+    public LayerMask WhatIsGround;
+
+    public bool IsGrounded => _isGrounded;
+
+    private const float _groudCheckRadius = 0.1f;
+    private bool _isGrounded;
+    private bool _wasGrounded;
+
+    public static Action OnLanding;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        CheckGround();
+    }
+
+    void CheckGround()
+    {
+        var overlap = Physics2D.OverlapCircle(GroundCheckPoint.position, _groudCheckRadius, WhatIsGround);
+        _isGrounded = overlap != null;
+
+        if (_isGrounded && !_wasGrounded)
+        {
+            OnLanding?.Invoke();
+        }
+
+        _wasGrounded = _isGrounded;
+    }
+
+    public float DistanceToGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, 100, WhatIsGround);
+        return hit.distance;
+    }
+}
--- Assets/_Scripts/PlayerJumper.cs
+++ Assets/_Scripts/PlayerJumper.cs
@@ -1,18 +1,120 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerJumper : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(GroundChecker))]
+[ExecuteInEditMode]
+public class Jumper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _rigidbody;
+
+
+    public float JumpHeight = 6;
+    public float TimeToPeak = 1;
+
+    public float MaxTimePressButton = 0.5f;
+
+    private GroundChecker _groundChecker;
+
+    float DistanceToGround => _groundChecker.DistanceToGround();
+
+    float _lastVelocityY;
+
+    float _buttonPressedTime;
+
+
+    private void OnEnable()
     {
-        
+        GroundChecker.OnLanding += OnLanding;
+    }
+    private void OnDisable()
+    {
+        GroundChecker.OnLanding -= OnLanding;
+    }
+
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        float h = -DistanceToGround + JumpHeight;
+        Vector3 start = transform.position + new Vector3(-1, h, 0);
+        Vector3 end = transform.position + new Vector3(+1, h, 0);
+        Gizmos.DrawLine(start, end);
+        Gizmos.color = Color.white;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _groundChecker = GetComponent<GroundChecker>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (OnZenit())
+            TweakGravity();
+
+        _lastVelocityY = _rigidbody.velocity.y;
+    }
+
+    void OnJump()
+    {
+        if (_groundChecker.IsGrounded)
+            Jump();
+    }
+
+    void OnJumpStarted()
+    {
+        _buttonPressedTime = Time.time;
+        OnJump();
+    }
+    void OnJumpFinished()
+    {
+        float pressedTime = Time.time - _buttonPressedTime;
+        float f = Mathf.Clamp01(pressedTime / MaxTimePressButton);
+        TweakGravityLongJump(f);
+    }
+
+    private void Jump()
+    {
+        ApplyGravity();
+        _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, GetStartSpeed());
+    }
+
+    private float GetStartSpeed()
+    {
+        return 2 * JumpHeight / TimeToPeak;
+    }
+
+    void ApplyGravity()
+    {
+        _rigidbody.gravityScale = -2 * JumpHeight / (TimeToPeak * TimeToPeak) / Physics2D.gravity.y;
+    }
+
+    void TweakGravity()
+    {
+        _rigidbody.gravityScale *= 2f;
+    }
+
+    void TweakGravityLongJump(float f)
+    {
+        _rigidbody.gravityScale *= 1 / f;
+    }
+
+    bool OnZenit()
+    {
+        return (_lastVelocityY > 0 && _rigidbody.velocity.y <= 0f); //4.0000 = 4.000000000000001; f1==f2 abs(f1-f2) < epsilon
+    }
+
+    private void OnLanding()
+    {
+        _rigidbody.gravityScale = 1;
     }
 }
